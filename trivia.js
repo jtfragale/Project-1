702807ce6fff5157c075a36687ede1502b2d5478
Jonathan Fragale <jtfragale@gmail.com>
2018-02-13 14:19:22 -0500

    started js

--- trivia.js
+++ trivia.js
@@ -1,1 +1,84 @@
-console.log('hello world')
+// console.log('hello world') test to make sure js linked properly
+
+
+
+
+var question1 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question2 = {
+    question:""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""   
+}
+
+var question3 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question4 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question5 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question6 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question7 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question8 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question9 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
+
+var question10 = {
+    question: ""
+    answerA: ""
+    answerB: ""
+    answerC: ""
+    answerD: ""
+}
