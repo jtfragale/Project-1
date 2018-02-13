cb9eea8a2af4412ae4869d8d4364edd19b62b350
Jonathan Fragale <jtfragale@gmail.com>
2018-02-13 15:21:28 -0500

    updated questions array

--- trivia.js
+++ trivia.js
@@ -1,84 +1,121 @@
 // console.log('hello world') test to make sure js linked properly
 
+// var gameQuestion = document.querySelector('question')
+// var gameAnswerA = document.querySelector('optionA')
+// var gameAsnwerB = document.querySelector('optionB')
+// var gameAnswerC = document.querySelector('optionC')
+// var gameAnswerD = document.querySelector('optionD')
+// var gameButton = document.querySelector('button')
 
+// gameButton.addEventListener('click', startGame)
 
+/*
+1. need some way of tracking which question we're on, which
+will correlate to the index for the curent question in our
+questions array.
+2. even listener for the options that checks if the answer is correct,
+updates the score, moves to next question (increment the index value and
+fill in the page with the new question and answers)
+3. make event listener for container-two and then target answer options
+*/
 
+// function startGame () {}
+
+// test to see if button is working
+// function startGame () {
+//     console.log('game started')
+// } 
+
+// make into array
+const  gameQuestions [
+{
 var question1 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{
 var question2 = {
     question:""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""   
-}
-
+    }
+},
+{
 var question3 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{
 var question4 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{
 var question5 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{
 var question6 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{
 var question7 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{    
 var question8 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{
 var question9 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
-}
-
+    }
+},
+{
 var question10 = {
     question: ""
     answerA: ""
     answerB: ""
     answerC: ""
     answerD: ""
+    }
 }
+]
