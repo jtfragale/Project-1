664ca5d44253d058e7fb64312740a07cebc3ad46
Jonathan Fragale <jtfragale@gmail.com>
2018-02-14 09:40:41 -0500

    added questions

--- trivia.js
+++ trivia.js
@@ -1,14 +1,4 @@
 // console.log('hello world') test to make sure js linked properly
-let currentQuestionIndex = 0;
-
-var gameQuestion = document.querySelector('.question')
-var gameAnswerA = document.querySelector('.optionA')
-var gameAsnwerB = document.querySelector('.optionB')
-var gameAnswerC = document.querySelector('.optionC')
-var gameAnswerD = document.querySelector('.optionD')
-var gameButton = document.querySelector('button')
-
-
 
 /*
 1. need some way of tracking which question we're on, which
@@ -27,6 +17,68 @@ fill in the page with the new question and answers)
 //     console.log('game started')
 // } 
 
+const gameQuestions = [
+    {
+        question: "Dory, or this type of fish, helped Marlin rescue Nemo in 'Finding Nemo', but probably doesn't remember",
+        options: [
+            "Clownfish",
+            "Blue Hippo Tang",
+            "Shark",
+            "Pufferfish"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "What is the largest living cartilaginous fish?",
+        options: [
+            "Whale Shark",
+            "Blue Whale",
+            "Great White Shark",
+            "Giant Manta Ray"
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "This type of fish has a beak that helps it grind up coral and coralline algae and as a result poops sand",
+        options: [
+            "Squirrelfish",
+            "Engineer Goby",
+            "Parrotfish",
+            "Jawfish"
+    ],
+        correctAnswer: 2
+    },
+    {
+        question: "This fish forms a symbiotic relationship with sea anemones, where it spends most of its life. It's pretty hilarious ",
+        options: [
+            "Dracula Goby",
+            "Cardinalfish",
+            "Clownfish",
+            "Squirrelfish"
+    ],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the largest living bony fish?",
+        options: [
+            "Giant Squid",
+            "Great White Shark",
+            "Giant Trevally",
+            "Sunfish"
+    ],
+        correctAnswer: 3
+    }
+]
+
+let currentQuestionIndex = 0;
+
+var gameQuestion = document.querySelector('.question')
+var gameAnswerA = document.querySelector('.optionA')
+var gameAsnwerB = document.querySelector('.optionB')
+var gameAnswerC = document.querySelector('.optionC')
+var gameAnswerD = document.querySelector('.optionD')
+var gameButton = document.querySelector('button')
+
 gameButton.addEventListener('click', startGame)
 
 function startGame () {
@@ -36,17 +88,3 @@ function startGame () {
     gameAnswerC.innerHTML = gameQuestions[currentQuestionIndex].options[2]
     gameAnswerD.innerHTML = gameQuestions[currentQuestionIndex].options[3]
 }
-
-
-const gameQuestions = [
-    {
-        question: "",
-        options: [
-            "1",
-            "2",
-            "3",
-            "4"
-        ],
-        correctAnswer: 3
-    }
-]
