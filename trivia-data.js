6ad61ba72e5a09e582d165e9073af65af837c717
Jonathan Fragale <jtfragale@gmail.com>
2018-02-14 15:05:31 -0500

    fixed some css

---
+++ trivia-data.js
@@ -0,0 +1,52 @@
+const gameQuestions = [
+    {
+        question: "Dory, or this type of fish, helped Marlin rescue Nemo in 'Finding Nemo', but probably doesn't remember",
+        options: [
+            "Clownfish",
+            "Blue Hippo Tang",
+            "Shark",
+            "Pufferfish"
+        ],
+        correctAnswer: "Blue Hippo Tang"
+    },
+    {
+        question: "What is the largest living cartilaginous fish?",
+        options: [
+            "Whale Shark",
+            "Blue Whale",
+            "Great White Shark",
+            "Giant Manta Ray"
+        ],
+        correctAnswer: "Whale Shark"
+    },
+    {
+        question: "This type of fish has a beak that helps it grind up coral and coralline algae and as a result poops sand",
+        options: [
+            "Squirrelfish",
+            "Engineer Goby",
+            "Parrotfish",
+            "Jawfish"
+    ],
+        correctAnswer: "Parrotfish"
+    },
+    {
+        question: "This fish forms a symbiotic relationship with sea anemones, where it spends most of its life. It's pretty hilarious.",
+        options: [
+            "Dracula Goby",
+            "Cardinalfish",
+            "Clownfish",
+            "Squirrelfish"
+    ],
+        correctAnswer: "Clownfish"
+    },
+    {
+        question: "What is the largest living bony fish?",
+        options: [
+            "Giant Squid",
+            "Great White Shark",
+            "Giant Trevally",
+            "Sunfish"
+    ],
+        correctAnswer: "Sunfish"
+    }
+]
--- trivia.js
+++ trivia.js
@@ -17,62 +17,62 @@ fill in the page with the new question and answers)
 //     console.log('game started')
 // } 
 
-const gameQuestions = [
-    {
-        question: "Dory, or this type of fish, helped Marlin rescue Nemo in 'Finding Nemo', but probably doesn't remember",
-        options: [
-            "Clownfish",
-            "Blue Hippo Tang",
-            "Shark",
-            "Pufferfish"
-        ],
-        correctAnswer: "Blue Hippo Tang"
-    },
-    {
-        question: "What is the largest living cartilaginous fish?",
-        options: [
-            "Whale Shark",
-            "Blue Whale",
-            "Great White Shark",
-            "Giant Manta Ray"
-        ],
-        correctAnswer: "Whale Shark"
-    },
-    {
-        question: "This type of fish has a beak that helps it grind up coral and coralline algae and as a result poops sand",
-        options: [
-            "Squirrelfish",
-            "Engineer Goby",
-            "Parrotfish",
-            "Jawfish"
-    ],
-        correctAnswer: "Parrotfish"
-    },
-    {
-        question: "This fish forms a symbiotic relationship with sea anemones, where it spends most of its life. It's pretty hilarious ",
-        options: [
-            "Dracula Goby",
-            "Cardinalfish",
-            "Clownfish",
-            "Squirrelfish"
-    ],
-        correctAnswer: "Clownfish"
-    },
-    {
-        question: "What is the largest living bony fish?",
-        options: [
-            "Giant Squid",
-            "Great White Shark",
-            "Giant Trevally",
-            "Sunfish"
-    ],
-        correctAnswer: "Sunfish"
-    }
-]
+// const gameQuestions = [
+//     {
+//         question: "Dory, or this type of fish, helped Marlin rescue Nemo in 'Finding Nemo', but probably doesn't remember",
+//         options: [
+//             "Clownfish",
+//             "Blue Hippo Tang",
+//             "Shark",
+//             "Pufferfish"
+//         ],
+//         correctAnswer: "Blue Hippo Tang"
+//     },
+//     {
+//         question: "What is the largest living cartilaginous fish?",
+//         options: [
+//             "Whale Shark",
+//             "Blue Whale",
+//             "Great White Shark",
+//             "Giant Manta Ray"
+//         ],
+//         correctAnswer: "Whale Shark"
+//     },
+//     {
+//         question: "This type of fish has a beak that helps it grind up coral and coralline algae and as a result poops sand",
+//         options: [
+//             "Squirrelfish",
+//             "Engineer Goby",
+//             "Parrotfish",
+//             "Jawfish"
+//     ],
+//         correctAnswer: "Parrotfish"
+//     },
+//     {
+//         question: "This fish forms a symbiotic relationship with sea anemones, where it spends most of its life. It's pretty hilarious.",
+//         options: [
+//             "Dracula Goby",
+//             "Cardinalfish",
+//             "Clownfish",
+//             "Squirrelfish"
+//     ],
+//         correctAnswer: "Clownfish"
+//     },
+//     {
+//         question: "What is the largest living bony fish?",
+//         options: [
+//             "Giant Squid",
+//             "Great White Shark",
+//             "Giant Trevally",
+//             "Sunfish"
+//     ],
+//         correctAnswer: "Sunfish"
+//     }
+// ]
 
 // let currentQuestionIndex = i;
-let i = 0;
 // var for score
+let i = 0;
 
 var gameQuestion = document.querySelector('.question')
 var gameAnswerA = document.querySelector('.optionA')
@@ -99,8 +99,8 @@ gameAnswerD.addEventListener('click', selectAnswer)
 
 function selectAnswer () {
         // console.log(this.innerHTML)
-        // console.log(gameQuestions[currentQuestionIndex].options)
-        // console.log(gameQuestions[currentQuestionIndex].correctAnswer)
+        // console.log(gameQuestions[i].options)
+        // console.log(gameQuestions[i].correctAnswer)
     if (this.innerHTML === gameQuestions[i].correctAnswer) {
         alert('Correct!')
         i++   
@@ -109,10 +109,7 @@ function selectAnswer () {
     } else {
         alert('Try Again')
     }
-    
     startGame()
-    // increase currentQuestionIndex
+    // increase i
     // startGame()
 }
-
-// 
