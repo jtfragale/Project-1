df8d7c48353f66c816fe276bb978c0134307a521
Jonathan Fragale <jtfragale@gmail.com>
2018-02-13 21:30:03 -0500

    added first question function

--- trivia.js
+++ trivia.js
@@ -1,19 +1,20 @@
 // console.log('hello world') test to make sure js linked properly
+let currentQuestionIndex = 0;
 
-// var gameQuestion = document.querySelector('question')
-// var gameAnswerA = document.querySelector('optionA')
-// var gameAsnwerB = document.querySelector('optionB')
-// var gameAnswerC = document.querySelector('optionC')
-// var gameAnswerD = document.querySelector('optionD')
-// var gameButton = document.querySelector('button')
+var gameQuestion = document.querySelector('.question')
+var gameAnswerA = document.querySelector('.optionA')
+var gameAsnwerB = document.querySelector('.optionB')
+var gameAnswerC = document.querySelector('.optionC')
+var gameAnswerD = document.querySelector('.optionD')
+var gameButton = document.querySelector('button')
 
-// gameButton.addEventListener('click', startGame)
+
 
 /*
 1. need some way of tracking which question we're on, which
 will correlate to the index for the curent question in our
 questions array.
-2. even listener for the options that checks if the answer is correct,
+2. event listener for the options that checks if the answer is correct,
 updates the score, moves to next question (increment the index value and
 fill in the page with the new question and answers)
 3. make event listener for container-two and then target answer options
@@ -26,96 +27,26 @@ fill in the page with the new question and answers)
 //     console.log('game started')
 // } 
 
-// make into array
-const  gameQuestions [
-{
-var question1 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{
-var question2 = {
-    question:""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""   
-    }
-},
-{
-var question3 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{
-var question4 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{
-var question5 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{
-var question6 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{
-var question7 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{    
-var question8 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{
-var question9 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
-},
-{
-var question10 = {
-    question: ""
-    answerA: ""
-    answerB: ""
-    answerC: ""
-    answerD: ""
-    }
+gameButton.addEventListener('click', startGame)
+
+function startGame () {
+    gameQuestion.innerHTML = gameQuestions[currentQuestionIndex].question
+    gameAnswerA.innerHTML = gameQuestions[currentQuestionIndex].options[0]
+    gameAsnwerB.innerHTML = gameQuestions[currentQuestionIndex].options[1]
+    gameAnswerC.innerHTML = gameQuestions[currentQuestionIndex].options[2]
+    gameAnswerD.innerHTML = gameQuestions[currentQuestionIndex].options[3]
 }
+
+
+const gameQuestions = [
+    {
+        question: "",
+        options: [
+            "1",
+            "2",
+            "3",
+            "4"
+        ],
+        correctAnswer: 3
+    }
 ]
