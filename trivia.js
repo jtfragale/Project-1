7d83c129fa1e2ba2a658e6da6db462b84df0d328
Jonathan Fragale <jtfragale@gmail.com>
2018-05-01 16:05:51 -0400

    removed enter name

--- trivia.js
+++ trivia.js
@@ -1,52 +1,50 @@
-(function iffe() {
-    
-let i = 0;
+;(function iffe() {
+  let i = 0
 
-var gameQuestion = document.querySelector('.question')
-var gameAnswerA = document.querySelector('.optionA')
-var gameAsnwerB = document.querySelector('.optionB')
-var gameAnswerC = document.querySelector('.optionC')
-var gameAnswerD = document.querySelector('.optionD')
-var gameButton = document.querySelector('button')
-var answerButton = document.querySelector('.container-two')
-var enterName = document.querySelector('.insert-name')
-var enterScore = document.querySelector('.insert-score')
+  var gameQuestion = document.querySelector('.question')
+  var gameAnswerA = document.querySelector('.optionA')
+  var gameAsnwerB = document.querySelector('.optionB')
+  var gameAnswerC = document.querySelector('.optionC')
+  var gameAnswerD = document.querySelector('.optionD')
+  var gameButton = document.querySelector('button')
+  var answerButton = document.querySelector('.container-two')
+  var enterName = document.querySelector('.insert-name')
+  var enterScore = document.querySelector('.insert-score')
 
-var name = prompt('Hello! Please Enter Your Name') || ''
-var score = 0
+  //   var name = prompt('Hello! Please Enter Your Name') || ''
+  var score = 0
 
-enterName.innerText = name
+  enterName.innerText = name
 
+  gameButton.addEventListener('click', startGame)
 
-gameButton.addEventListener('click', startGame)
-
-function startGame () {
+  function startGame() {
     gameQuestion.innerHTML = gameQuestions[i].question
     gameAnswerA.innerHTML = gameQuestions[i].options[0]
     gameAsnwerB.innerHTML = gameQuestions[i].options[1]
     gameAnswerC.innerHTML = gameQuestions[i].options[2]
     gameAnswerD.innerHTML = gameQuestions[i].options[3]
-}
+  }
 
-gameAnswerA.addEventListener('click', selectAnswer)
-gameAsnwerB.addEventListener('click', selectAnswer)
-gameAnswerC.addEventListener('click', selectAnswer)
-gameAnswerD.addEventListener('click', selectAnswer)
+  gameAnswerA.addEventListener('click', selectAnswer)
+  gameAsnwerB.addEventListener('click', selectAnswer)
+  gameAnswerC.addEventListener('click', selectAnswer)
+  gameAnswerD.addEventListener('click', selectAnswer)
 
-function selectAnswer () {
+  function selectAnswer() {
     if (this.innerHTML === gameQuestions[i].correctAnswer) {
-        alert('Correct!')
-        i++   
-        score += 1
-        enterScore.innerText = score
+      alert('Correct!')
+      i++
+      score += 1
+      enterScore.innerText = score
     } else {
-        alert('Wrong')
-        i++
+      alert('Wrong')
+      i++
     }
-    if( i < gameQuestions.length) {
-        startGame()  
+    if (i < gameQuestions.length) {
+      startGame()
     } else {
-        alert('Game Over!')
+      alert('Game Over!')
     }
-}
+  }
 })()
